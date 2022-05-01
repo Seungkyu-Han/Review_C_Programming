4405e4e3bd303544006402f62b36bd2f9e6d51f1
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2022-05-02 00:40:28 +0900

    Add files via upload

---
+++ 2-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h> //전처리기
+
+int main(void){
+
+    puts("Third C Programming");
+    puts("");
+    printf("--- Part of Visual Studio Code C programming ---");
+    printf("\n");
+    puts("1. Creating Solution and Project");
+    printf("2. Edit Source File\n");
+    printf("3. execute\n");
+
+    return 0;
+}
